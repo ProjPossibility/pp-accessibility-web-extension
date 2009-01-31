c2815a7c0ddd87a69ddd1999695af8f3873b4e38
gauyeung <gauyeung@75c26b48-679a-11de-bf08-17995f091a54>
2009-01-31 21:30:36 +0000

    Initial import

---
+++ defaults/preferences/siteannotator.js
@@ -0,0 +1,2 @@
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.siteannotator@handicap.annotate.description", "chrome://siteannotator/locale/siteannotator.properties");
