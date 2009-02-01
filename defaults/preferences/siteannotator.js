cc2d54e9f1bfc649dd187d5b97fb5bdec79def23
gauyeung <gauyeung@75c26b48-679a-11de-bf08-17995f091a54>
2009-02-01 00:21:41 +0000

    clean out

--- defaults/preferences/siteannotator.js
+++
@@ -1,2 +0,0 @@
-// See http://kb.mozillazine.org/Localize_extension_descriptions
-pref("extensions.siteannotator@handicap.annotate.description", "chrome://siteannotator/locale/siteannotator.properties");
